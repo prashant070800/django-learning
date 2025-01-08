f78991d58497b64e05cb882ddb4d4c12137f9f7d
prashant singh <singhprashant070800@gmail.com>
2025-01-08 23:59:05 +0530

    add some comment

--- reaction/rmodel/serializer.py
+++ reaction/rmodel/serializer.py
@@ -5,3 +5,33 @@ class ReactionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reaction
         fields = ['id', 'unicode_character']
+
+
+
+# from rest_framework import serializers
+# from django.core.exceptions import ValidationError
+# from .models import Reaction
+# import unicodedata
+
+# def validate_unicode(value):
+#     """
+#     Validate if the given value is a valid Unicode character.
+#     """
+#     try:
+#         if value.startswith("\\U") or value.startswith("\\u"):
+#             value = value.encode().decode('unicode_escape')
+#         unicodedata.name(value)  # Validate the Unicode character
+#     except (ValueError, UnicodeDecodeError):
+#         raise ValidationError(f"{value} is not a valid Unicode character.")
+#     return value
+
+# class ReactionSerializer(serializers.ModelSerializer):
+#     unicode_character = serializers.CharField(
+#         max_length=10, 
+#         validators=[validate_unicode]
+#     )
+
+#     class Meta:
+#         model = Reaction
+#         fields = ['id', 'unicode_character']
+
